1ff67d3946fbd51c4e64180234bd5e83e25c6285
Edgar Dewsbery <edewsbery@gmail.com>
2017-12-30 12:25:46 +0000

    express api initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api', (req, res) => {
+
+  // Run selection logic
+
+
+  // Return
+  res.send('Welcome to API');
+});
+
+
+// "Catchall handler: send React index.html for any other requests than above"
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Rota generator listening on ${port}`);
