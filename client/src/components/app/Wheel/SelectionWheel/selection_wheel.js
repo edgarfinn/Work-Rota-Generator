5749fd57bec9ea95d2bf1f5bd857d27052cfbec9
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-09 16:53:55 +0000

    feed wheel selection from props so it updates when devList updates

--- client/src/components/app/Wheel/SelectionWheel/selection_wheel.js
+++ client/src/components/app/Wheel/SelectionWheel/selection_wheel.js
@@ -3,32 +3,39 @@ import getDevName from '../../Helpers/lookup_dev'
 
 import './selection_wheel.css';
 
-export default(props) => {
-  const selectionAM = props.selections.selectionAM;
-  const selectionPM = props.selections.selectionPM;
+export default (props) => {
 
-  const selectionsEmpty = !props.selectionIsReady;
+  console.log(props);
 
-  if (props.selectionIsReady) {
-    const AMDevKey = props.selections.morning.devKey;
-    const PMDevKey = props.selections.afternoon.devKey;
-    const AMDevName = getDevName(AMDevKey,props.devList) || AMDevKey;
-    const PMDevName = getDevName(PMDevKey,props.devList) || PMDevKey;
+  const AMDev = () => {
+    if (props.selectionIsReady) {
+      const AMDevKey = props.selections.morning.devKey;
+      return getDevName(AMDevKey,props.devList) || AMDevKey;
+    }
   }
 
+  const PMDev = () => {
+    if (props.selectionIsReady) {
+      const PMDevKey = props.selections.afternoon.devKey;
+      return getDevName(PMDevKey,props.devList) || PMDevKey;
+    }
+  }
+
+
+  const selectionsEmpty = !props.selectionIsReady;
   return (
     <div className="wheel-selection-wheel">
       <p
         className={selectionsEmpty ? "selection morning" : "selection morning selected"}
         >
-          {selectionsEmpty ? "Select a developer" : AMDevName }
-      </p>
-      <p
-        className={selectionsEmpty ? "selection afternoon" : "selection afternoon selected"}
-        >
-          {selectionsEmpty ? "using the buttons below" : PMDevName }
+          {selectionsEmpty ? "Select a developer" : <AMDev />}
+        </p>
+        <p
+          className={selectionsEmpty ? "selection afternoon" : "selection afternoon selected"}
+          >
+            {selectionsEmpty ? "using the buttons below" : <PMDev />}
+          </p>
+        </div>
+      )
 
-      </p>
-    </div>
-  )
 }
