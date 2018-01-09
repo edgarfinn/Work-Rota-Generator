90814794aa07ba14a7f184762e5ffcac18b4f056
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-09 16:09:37 +0000

    add devname lookup

---
+++ client/src/components/app/Helpers/update_rota_object.js
@@ -0,0 +1,22 @@
+import findRotaIndices from './find_rota_indices';
+
+export default (dayID,rota,selection) => {
+
+  const newRota = JSON.parse(JSON.stringify(rota))
+  
+  const dayIndex = findRotaIndices(dayID).dayIndex;
+  const weekIndex = findRotaIndices(dayID).weekIndex;
+
+  // identify day to be updated
+  const dayToUpdate = newRota[weekIndex].schedule[dayIndex(dayID)];
+
+  // identify period to be updated
+  const morningUpdate = dayToUpdate.periods[0];
+  const afternoonUpdate = dayToUpdate.periods[1];
+
+  // set allocations for each peiord
+  morningUpdate.allocation = selection.selectionAM;
+  afternoonUpdate.allocation = selection.selectionPM;
+
+  return newRota;
+}
